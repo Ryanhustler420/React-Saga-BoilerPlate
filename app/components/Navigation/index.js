fe1fa3386666565ed5ad99280fbc6286dc37a096
Gaurav Gupta <gouravgupta840@gmail.com>
2019-06-17 11:21:50 +0530

    Getting-Data-from-the-Store-on-to-Component

--- app/components/Navigation/index.js
+++ app/components/Navigation/index.js
@@ -9,12 +9,21 @@ import React from 'react';
 
 import styles from './styles.css';
 
-function Navigation() {
+function Navigation({ topics }) {
   return (
     <div className={styles.navigation}>
-      This is the navigation component
+      we have {topics.length} topics in the nav component
     </div>
   );
+}
+
+Navigation.propTypes = {
+  topics: React.PropTypes.arrayOf(
+    React.PropTypes.shape({
+      name: React.PropTypes.string.isRequired,
+      description: React.PropTypes.string.isRequired
+    })
+  ).isRequired,
 }
 
 export default Navigation;
