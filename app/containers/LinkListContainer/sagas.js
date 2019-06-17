6fd40fca2d4ab1257a7bc3c632627f0d4fa1fdd8
Gaurav Gupta <gouravgupta840@gmail.com>
2019-06-17 21:11:16 +0530

    Get-Data-from-the-Server-Based-on-User-Input

--- app/containers/LinkListContainer/sagas.js
+++ app/containers/LinkListContainer/sagas.js
@@ -1,8 +1,27 @@
 // import { take, call, put, select } from 'redux-saga/effects';
+import { call } from 'redux-saga/effects';
+import { takeLatest } from 'redux-saga';
+import { SELECT_TOPIC } from '../NavigationContainer/constants';
+
+function fetchLinksFromServer(topic) {
+  return fetch(`http://localhost:3000/api/topics/${topic.name}/links`)
+    .then(response => response.json());
+}
+
+function* fetchLinks(action) {
+  try{
+    const links = yield call(fetchLinksFromServer, action.topic)
+    // dispatch action to store links
+    console.log("LINKS FROM SERVER:", links);
+  }catch(e) {
+    console.log(e);
+    // dispatch action to store error
+  }
+}
 
 // Individual exports for testing
 export function* defaultSaga() {
-  return;
+  yield* takeLatest(SELECT_TOPIC, fetchLinks)
 }
 
 // All sagas to be loaded
