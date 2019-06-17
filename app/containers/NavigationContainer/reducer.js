3b34010c45a7c419e1cee8769c37625949110cbf
Gaurav Gupta <gouravgupta840@gmail.com>
2019-06-17 12:43:57 +0530

    Storing-Data-on-the-Redux-Store-from-a-Saga

--- app/containers/NavigationContainer/reducer.js
+++ app/containers/NavigationContainer/reducer.js
@@ -5,27 +5,18 @@
  */
 
 import { fromJS } from 'immutable';
-import { } from './constants';
+import {
+  REQUEST_TOPICS_SUCCEEDED,
+} from './constants';
 
 const initialState = fromJS({
-  topics: [
-    {
-      name: 'libraries',
-      description: 'links to useful open source libraries',
-    },
-    {
-      name: 'apps',
-      description: 'links to new and exciting apps',
-    },
-    {
-      name: 'news',
-      description: 'links to programming related news articles',
-    },
-  ]
+  topics: [],
 });
 
 function navigationContainerReducer(state = initialState, action) {
   switch (action.type) {
+    case REQUEST_TOPICS_SUCCEEDED:
+      return state.set('topics', action.topics);
     default:
       return state;
   }
--- app/containers/NavigationContainer/sagas.js
+++ app/containers/NavigationContainer/sagas.js
@@ -1,16 +1,24 @@
 // import { take, call, put, select } from 'redux-saga/effects';
 import { REQUEST_TOPICS } from './constants';
 import { takeLatest } from 'redux-saga';
-import { call } from 'redux-saga/effects';
+import { call, put } from 'redux-saga/effects';
+import { requestTopicsSucceeded, requestTopicsFailed } from './actions';
 
 export function fetchTopicsFromServer() {
   return fetch('http://localhost:3000/api/topics')
       .then(response => response.json());
 }
 
+
+
 function* fetchTopics() {
-  const topics = yield call(fetchTopicsFromServer);
-  console.log("TOPICS FROM THE SERVER!!!", topics);
+  try {
+    const topics = yield call(fetchTopicsFromServer);
+    yield put(requestTopicsSucceeded(topics));
+  }catch(e) {
+    yield put(requestTopicsFailed(e.message))
+  }
+
 }
 
 // Individual exports for testing
