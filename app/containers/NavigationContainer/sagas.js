7e6c66efaf383fb5fc955a39015e5d2423fd84fa
Gaurav Gupta <gouravgupta840@gmail.com>
2019-06-17 12:26:58 +0530

    Intercepting-Actions-in-a-Saga-to-Execute-Logic

--- app/containers/NavigationContainer/sagas.js
+++ app/containers/NavigationContainer/sagas.js
@@ -1,11 +1,13 @@
 // import { take, call, put, select } from 'redux-saga/effects';
+import { REQUEST_TOPICS } from './constants';
+import { takeLatest } from 'redux-saga';
 
 // Individual exports for testing
-export function* defaultSaga() {
-  return;
+export function* fetchTopicsSaga() {
+  yield* takeLatest(REQUEST_TOPICS, () => {console.log('LOGGED BY SAGA!!!')});
 }
 
 // All sagas to be loaded
 export default [
-  defaultSaga,
+  fetchTopicsSaga,
 ];
