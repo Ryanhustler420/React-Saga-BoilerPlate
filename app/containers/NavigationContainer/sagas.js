04dd8d9194fea96232584998dab2652f3ff97957
Gaurav Gupta <gouravgupta840@gmail.com>
2019-06-17 12:33:18 +0530

    Fetching-Server-Data-from-a-Saga

--- app/containers/NavigationContainer/sagas.js
+++ app/containers/NavigationContainer/sagas.js
@@ -1,10 +1,21 @@
 // import { take, call, put, select } from 'redux-saga/effects';
 import { REQUEST_TOPICS } from './constants';
 import { takeLatest } from 'redux-saga';
+import { call } from 'redux-saga/effects';
+
+export function fetchTopicsFromServer() {
+  return fetch('http://localhost:3000/api/topics')
+      .then(response => response.json());
+}
+
+function* fetchTopics() {
+  const topics = yield call(fetchTopicsFromServer);
+  console.log("TOPICS FROM THE SERVER!!!", topics);
+}
 
 // Individual exports for testing
 export function* fetchTopicsSaga() {
-  yield* takeLatest(REQUEST_TOPICS, () => {console.log('LOGGED BY SAGA!!!')});
+  yield* takeLatest(REQUEST_TOPICS, fetchTopics);
 }
 
 // All sagas to be loaded
