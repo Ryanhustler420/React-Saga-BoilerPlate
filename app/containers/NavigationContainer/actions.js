38eab5e8477e3ce4e81a95435d283cd81966dbd7
Gaurav Gupta <gouravgupta840@gmail.com>
2019-06-17 18:29:36 +0530

    Storing-User-Input-on-the-Redux-Store

--- app/containers/NavigationContainer/actions.js
+++ app/containers/NavigationContainer/actions.js
@@ -8,6 +8,7 @@ import {
   REQUEST_TOPICS,
   REQUEST_TOPICS_SUCCEEDED,
   REQUEST_TOPICS_FAILED,
+  SELECT_TOPIC
 } from './constants';
 
 export function requestTopics () {
@@ -27,5 +28,12 @@ export function requestTopicsFailed(message) {
   return {
     type: REQUEST_TOPICS_FAILED,
     message,
+  }
+}
+
+export function selectTopic(topic) {
+  return {
+    type: SELECT_TOPIC,
+    topic
   }
 }
--- app/containers/NavigationContainer/reducer.js
+++ app/containers/NavigationContainer/reducer.js
@@ -7,6 +7,7 @@
 import { fromJS } from 'immutable';
 import {
   REQUEST_TOPICS_SUCCEEDED,
+  SELECT_TOPIC,
 } from './constants';
 
 const initialState = fromJS({
@@ -17,6 +18,8 @@ function navigationContainerReducer(state = initialState, action) {
   switch (action.type) {
     case REQUEST_TOPICS_SUCCEEDED:
       return state.set('topics', action.topics);
+    case SELECT_TOPIC:
+      return state.set('selectedTopic', action.topic);
     default:
       return state;
   }
