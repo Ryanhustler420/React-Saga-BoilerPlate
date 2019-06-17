0dd48d006e733c173cbabb17f0e3613f5ef94bc7
Gaurav Gupta <gouravgupta840@gmail.com>
2019-06-18 02:44:37 +0530

    Styling-the-Link-Component

--- app/components/Link/index.js
+++ app/components/Link/index.js
@@ -11,13 +11,28 @@ import styles from './styles.css';
 function Link({link}) {
   return (
     <div className={styles.link}>
-      {link.url}
+      <div className={styles.votingContainer}>
+        <div className={styles.votingCount}>
+          {link.voteCount}
+        </div>
+      </div>
+      <div className={styles.detailsContainer}>
+        <div>
+          <a href={link.url} className={styles.linkAnchor}>
+            {link.url}
+          </a>
+        </div>
+        <div className={styles.description}>
+          {link.description}
+        </div>
+      </div>
     </div>
   );
 }
 
 Link.propTypes = {
   link: React.PropTypes.shape ({
+    voteCount: React.PropTypes.number.isRequired,
     description: React.PropTypes.string.isRequired,
     url: React.PropTypes.string.isRequired,
     id: React.PropTypes.string.isRequired,
