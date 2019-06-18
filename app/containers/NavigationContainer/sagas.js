c797734b2cf9b182f8ddb53668c0e3e69d68c7d1
Gaurav Gupta <gouravgupta840@gmail.com>
2019-06-18 20:21:06 +0530

    Redirecting-to-a-Route-Based-on-Server-Data-When-the-App-Loads

--- app/containers/NavigationContainer/sagas.js
+++ app/containers/NavigationContainer/sagas.js
@@ -1,9 +1,10 @@
 // import { take, call, put, select } from 'redux-saga/effects';
-import { REQUEST_TOPICS, SELECT_TOPIC } from './constants';
+import { REQUEST_TOPICS, SELECT_TOPIC, REQUEST_TOPICS_SUCCEEDED } from './constants';
 import { takeLatest } from 'redux-saga';
-import { call, put } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
 import { requestTopicsSucceeded, requestTopicsFailed } from './actions';
 import { push } from 'react-router-redux';
+import selectNavigationContainer from './selectors';
 
 export function fetchTopicsFromServer() {
   return fetch('http://localhost:3000/api/topics')
@@ -26,6 +27,17 @@ function* pushTopic(action) {
   yield put(push(`/topic/${action.topic.name}`));
 }
 
+function* selectDefaultTopic() {
+  const state = yield select(selectNavigationContainer());
+  if(!state.selectedTopic){
+    yield put(push(`/topic/${state.topics[0].name}`));
+  }
+}
+
+function* selectDefaultTopicSaga() {
+  yield* takeLatest(REQUEST_TOPICS_SUCCEEDED, selectDefaultTopic);
+}
+
 // Individual exports for testing
 export function* fetchTopicsSaga() {
   yield* takeLatest(REQUEST_TOPICS, fetchTopics);
@@ -39,4 +51,5 @@ export function* selectTopicSaga() {
 export default [
   fetchTopicsSaga,
   selectTopicSaga,
+  selectDefaultTopicSaga,
 ];
