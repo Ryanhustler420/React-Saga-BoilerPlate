36c44c218ffe61a17bc4a8a1c64e4090005cde61
Gaurav Gupta <gouravgupta840@gmail.com>
2019-06-18 17:45:40 +0530

    Using-Reselect-to-Access-Route-Parameters

--- app/components/LinkList/index.js
+++ app/components/LinkList/index.js
@@ -10,18 +10,20 @@ import React from 'react';
 import styles from './styles.css';
 import Link from './../Link/index';
 
-function LinkList({ links }) {
+function LinkList({ links, routeTopicName }) {
 
   const linkNodes = links.map(l => <Link key={l.id} link={l}/>);
 
   return (
     <div className={styles.linkList}>
+      {routeTopicName}
       {linkNodes}
     </div>
   );
 }
 
 LinkList.propTypes = {
+  routeTopicName: React.PropTypes.string.isRequired,
   links: React.PropTypes.arrayOf(React.PropTypes.shape({
     description: React.PropTypes.string.isRequired,
     url: React.PropTypes.string.isRequired,
--- app/containers/LinkListContainer/selectors.js
+++ app/containers/LinkListContainer/selectors.js
@@ -9,6 +9,7 @@ const selectLinkListContainerDomain = () => state => state.get('linkListContaine
  * Other specific selectors
  */
 
+const selectRouteTopic = () => (state, props) => props.params.topicName;
 
 /**
  * Default selector used by LinkListContainer
@@ -16,7 +17,8 @@ const selectLinkListContainerDomain = () => state => state.get('linkListContaine
 
 const selectLinkListContainer = () => createSelector(
   selectLinkListContainerDomain(),
-  (substate) => substate.toJS()
+  selectRouteTopic(),
+  (substate, routeTopicName) => Object.assign(substate.toJS(),{ routeTopicName })
 );
 
 export default selectLinkListContainer;
