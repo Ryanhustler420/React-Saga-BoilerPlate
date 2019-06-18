9049e9f149e1fb2a041abf0ec3426f08446425de
Gaurav Gupta <gouravgupta840@gmail.com>
2019-06-18 15:24:16 +0530

    Getting-a-Component-to-Display-on-a-Child-Route

--- app/routes.js
+++ app/routes.js
@@ -25,8 +25,6 @@ export default function createRoutes(store) {
           System.import('containers/HomePage'),
           System.import('containers/NavigationContainer/reducer'),
           System.import('containers/NavigationContainer/sagas'),
-          System.import('containers/LinkListContainer/reducer'),
-          System.import('containers/LinkListContainer/sagas'),
         ]);
 
         const renderRoute = loadModule(cb);
@@ -35,38 +33,38 @@ export default function createRoutes(store) {
             component,
             navigationReducer,
             navigationSagas,
-            linkListReducer,
-            linkListSagas,
         ]) => {
           injectReducer('navigationContainer', navigationReducer.default);
           injectSagas('navigationContainer', navigationSagas.default);
-          injectReducer('linkListContainer', linkListReducer.default);
-          injectSagas('linkList', linkListSagas.default);
           renderRoute(component);
         });
 
         importModules.catch(errorLoading);
       },
-    }, {
-      path: '/topic/:topicName',
-      name: 'linkListContainer',
-      getComponent(nextState, cb) {
-        const importModules = Promise.all([
-          System.import('containers/LinkListContainer/reducer'),
-          System.import('containers/LinkListContainer/sagas'),
-          System.import('containers/LinkListContainer'),
-        ]);
+      childRoutes: [
+        {
+          path: '/topic/:topicName',
+          name: 'linkListContainer',
+          getComponent(nextState, cb) {
+            const importModules = Promise.all([
+              System.import('containers/LinkListContainer/reducer'),
+              System.import('containers/LinkListContainer/sagas'),
+              System.import('containers/LinkListContainer'),
+            ]);
 
-        const renderRoute = loadModule(cb);
+            const renderRoute = loadModule(cb);
 
-        importModules.then(([reducer, sagas, component]) => {
-          injectReducer('linkListContainer', reducer.default);
-          injectSagas(sagas.default);
-          renderRoute(component);
-        });
+            importModules.then(([reducer, sagas, component]) => {
+              injectReducer('linkListContainer', reducer.default);
+              // Change below line because we change the saga to singleton
+              injectSagas('linkListContainer', sagas.default);
+              renderRoute(component);
+            });
 
-        importModules.catch(errorLoading);
-      },
+            importModules.catch(errorLoading);
+          },
+        },
+      ]
     }, {
       path: '*',
       name: 'notfound',
