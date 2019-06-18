93c65a8fb1d89af990fa44f0bf43859591873489
Gaurav Gupta <gouravgupta840@gmail.com>
2019-06-18 14:22:07 +0530

    Intercepting-an-Action-to-Change-the-Route

--- app/containers/LinkListContainer/sagas.js
+++ app/containers/LinkListContainer/sagas.js
@@ -1,7 +1,6 @@
 // import { take, call, put, select } from 'redux-saga/effects';
 import { call, put } from 'redux-saga/effects';
-import { takeLatest } from 'redux-saga';
-import { SELECT_TOPIC } from '../NavigationContainer/constants';
+
 import { requestLinksSucceeded, requestLinksFailed } from './actions';
 
 function fetchLinksFromServer(topic) {
@@ -23,7 +22,7 @@ function* fetchLinks(action) {
 
 // Individual exports for testing
 export function* defaultSaga() {
-  yield* takeLatest(SELECT_TOPIC, fetchLinks)
+  
 }
 
 // All sagas to be loaded
--- app/containers/NavigationContainer/sagas.js
+++ app/containers/NavigationContainer/sagas.js
@@ -1,8 +1,9 @@
 // import { take, call, put, select } from 'redux-saga/effects';
-import { REQUEST_TOPICS } from './constants';
+import { REQUEST_TOPICS, SELECT_TOPIC } from './constants';
 import { takeLatest } from 'redux-saga';
 import { call, put } from 'redux-saga/effects';
 import { requestTopicsSucceeded, requestTopicsFailed } from './actions';
+import { push } from 'react-router-redux';
 
 export function fetchTopicsFromServer() {
   return fetch('http://localhost:3000/api/topics')
@@ -21,12 +22,21 @@ function* fetchTopics() {
 
 }
 
+function* pushTopic(action) {
+  yield put(push(`/topic/${action.topic.name}`));
+}
+
 // Individual exports for testing
 export function* fetchTopicsSaga() {
   yield* takeLatest(REQUEST_TOPICS, fetchTopics);
 }
 
+export function* selectTopicSaga() {
+  yield* takeLatest(SELECT_TOPIC, pushTopic);
+}
+
 // All sagas to be loaded
 export default [
   fetchTopicsSaga,
+  selectTopicSaga,
 ];
