55c53f83e83d400d0332a5f9bb27f150556c6f1e
Gaurav Gupta <gouravgupta840@gmail.com>
2019-06-19 11:43:08 +0530

    Navigating-Back-to-the-Previous-Route

--- app/containers/LoginContainer/sagas.js
+++ app/containers/LoginContainer/sagas.js
@@ -1,11 +1,19 @@
 // import { take, call, put, select } from 'redux-saga/effects';
+import { LOGIN } from './constants';
+import { put } from 'redux-saga/effects';
+import { takeLatest } from 'redux-saga';
+import { goBack } from 'react-router-redux';
 
 // Individual exports for testing
-export function* defaultSaga() {
-  return;
+function* handleLogin() {
+  yield put(goBack());
+}
+
+export function* doLoginSaga() {
+  yield* takeLatest(LOGIN, handleLogin)
 }
 
 // All sagas to be loaded
 export default [
-  defaultSaga,
+  doLoginSaga,
 ];
