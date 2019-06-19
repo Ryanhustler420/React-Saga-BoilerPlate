13d562c87a16e286f91efff4db96f1370de06a79
Gaurav Gupta <gouravgupta840@gmail.com>
2019-06-19 15:17:15 +0530

    Modify-New-Generic-Component-for-Text-Input

--- app/components/TextInput/index.js
+++ app/components/TextInput/index.js
@@ -7,14 +7,41 @@
 import React from 'react';
 
 import styles from './styles.css';
+import classNames from 'classnames';
 
-class TextInput extends React.Component { // eslint-disable-line react/prefer-stateless-function
-  render() {
+class TextInput extends React.Component {
+  // eslint-disable-line react/prefer-stateless-function
+  value () {
+    return this.field.value
+  }
+  
+  render () {
+    const {errorText} = this.props;
+
+    const fieldError = errorText
+      ? <div className={styles.errorMessage}>
+          {errorText}
+        </div>
+      : null;
+
     return (
-      <div className={styles.textInput}>
+      <div>
+        <input 
+          className={classNames(styles.input, this.props.className, {[styles.inputError]: errorText})}
+          placeholder={this.props.placeholder}
+          ref={f => {this.field = f;}}
+          type="text"
+        />
+        {fieldError}
       </div>
     );
   }
 }
+
+TextInput.propType = {
+  errorText: React.PropTypes.string,
+  placeholder: React.PropTypes.string,
+  className: React.PropTypes.string,
+};
 
 export default TextInput;
