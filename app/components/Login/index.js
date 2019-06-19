d2297ae5fa34242bc2c2a81614c818afee33b916
Gaurav Gupta <gouravgupta840@gmail.com>
2019-06-19 10:43:05 +0530

    Presenting-a-Validation-Message-on-a-Form

--- app/components/Login/index.js
+++ app/components/Login/index.js
@@ -8,14 +8,26 @@ import React from 'react';
 
 import styles from './styles.css';
 import validator from 'email-validator';
+import classNames from 'classnames';
 
 class Login extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  state = {
+    errorText: null
+  };
+
   login = () => {
     // work with bind
     const email = this.emailField.value;
     if(!validator.validate(email)){
-      console.log(`${this.emailField.value}!, Is Not An Valid Email Address`);
+      // console.log(`${this.emailField.value}!, Is Not An Valid Email Address`);
+      this.setState({
+        errorText: 'Please Provide A Valid Email'
+      });
+      return;
     }
+    this.setState({
+      errorText: null
+    })
   }
 
   // login() {
@@ -24,17 +36,23 @@ class Login extends React.Component { // eslint-disable-line react/prefer-statel
   // }
   
   render() {
+    const fieldError = this.state.errorText ? (
+      <div className={styles.errorMessage}>
+        {this.state.errorText}
+      </div>
+    ): null;
     return (
       <div className={styles.login}>
         <div className={styles.heading}>
             Login with your email
         </div>
         <input 
-          className={styles.input}
+          className={classNames(styles.input, { [styles.inputError]: this.state.errorText })}
           placeholder="Your email"
           ref={f => {this.emailField = f;}}
           type="text"
         />
+        {fieldError}
         <div className={styles.actionContainer}>
           <div className={styles.button}>
             cancel
