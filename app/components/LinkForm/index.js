65b0d16aa29599d6393f2825938dbbd132e68194
Gaurav Gupta <gouravgupta840@gmail.com>
2019-06-20 07:57:58 +0530

    Hooking-up-Validation-for-the-Link-Form

--- app/components/LinkForm/index.js
+++ app/components/LinkForm/index.js
@@ -11,16 +11,41 @@ import TextInput from './../TextInput/index';
 
 class LinkForm extends React.Component {
   // eslint-disable-line react/prefer-stateless-function
-  state = {};
+  state = {
+    urlError: '',
+    descriptionError: '',
+  };
+
+  onAdd = () => {
+    // getting the ref values
+    const url = this.url.value ();
+    const description = this.description.value ();
+    let urlError = null;
+    let descriptionError = null;
+
+    if (
+      !url.match (
+        /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
+      )
+    ) {
+      urlError = 'Please provide a valid URL';
+    }
+
+    if(!description) {
+      descriptionError = 'Please provide a valid description';
+    }
+
+    this.setState({
+      urlError,
+      descriptionError,
+    })
+
+    if(urlError || description){
+      return;
+    }
+  };
 
   render () {
-    const fieldError = () =>
-      this.state.errorText
-        ? <div className={styles.errorMessage}>
-            {this.state.errorText}
-          </div>
-        : null;
-
     return (
       <div className={styles.overlay}>
         <div className={styles.linkForm}>
@@ -28,17 +53,24 @@ class LinkForm extends React.Component {
             Add a link
           </div>
 
-          <TextInput placeholder="URL" className={styles.input} />
-          <TextInput placeholder="Description" className={styles.input} />
+          <TextInput
+            errorText={this.state.urlError}
+            ref={f => (this.url = f)}
+            placeholder="URL"
+            className={styles.input}
+          />
+          <TextInput
+            errorText={this.state.descriptionError}
+            ref={f => (this.description = f)}
+            placeholder="Description"
+            className={styles.input}
+          />
 
           <div className={styles.actionContainer}>
             <div className={styles.button} onClick={this.props.cancelLogin}>
               cancel
             </div>
-            <div
-              className={styles.button}
-              onClick={this.login}
-            >
+            <div className={styles.button} onClick={this.onAdd}>
               add
             </div>
           </div>
